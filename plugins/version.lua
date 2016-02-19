4afe8b0d9470506ba6b4a481f4b843818ed843e0
TeleDALAD <ashkankorouei@gmail.com>
2016-02-19 10:56:26 +0330

    Update version.lua

--- plugins/version.lua
+++ plugins/version.lua
@@ -1,25 +1,15 @@
 do
 
 function run(msg, matches)
-  return 'NOD32-BOT v5'.. VERSION .. [[ 
- 
-  Developer : @Behroozyaghi
-  
-  Channel : @Nod32team
-  
-  Open git : https://github.com/BH-YAGHI/NOD32-BOT.git
-  
-  NOD32 VERSION 5
-  
-  All rights reserved.
-  __________________]]
+  return 'mahdiyeh v1'.. VERSION .. [[بات شخصی مهدیه
+ورژن یک]]
 end
 
 return {
   description = "Shows bot version", 
   usage = "!version: Shows bot version",
   patterns = {
-    "^!version$"
+    "^[!/]version$"
   }, 
   run = run 
 }
