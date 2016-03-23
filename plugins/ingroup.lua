78f04e514b39e583ca93959e9fe0169dac239068
TeleDALAD <ashkankorouei@gmail.com>
2016-03-23 14:35:54 +0430

    Update ingroup.lua

--- plugins/ingroup.lua
+++ plugins/ingroup.lua
@@ -14,13 +14,15 @@ local function check_member_autorealm(cb_extra, success, result)
         settings = {
           set_name = string.gsub(msg.to.print_name, '_', ' '),
           lock_join = 'no',
-          antifosh = 'yes',
-	  antiads = 'yes',
-	  lock_name = 'yes',
+          antifosh = 'no',
+	  antiads = 'no',
+          antilink = 'no',
+	  lock_name = 'no',
           lock_photo = 'no',
           lock_member = 'no',
           lock_chat = 'no',
-          flood = 'yes'
+          lock_stickers = 'no',
+          flood = 'no'          
         }
       }
       save_data(_config.moderation.data, data)
@@ -48,13 +50,15 @@ local function check_member_realm_add(cb_extra, success, result)
         settings = {
           set_name = string.gsub(msg.to.print_name, '_', ' '),
           lock_join = 'no',
-		  antifosh = 'yes',
-		  antiads = 'yes',
-		  lock_name = 'yes',
+          antifosh = 'no',
+	  antiads = 'no',
+          antilink = 'no',
+	  lock_name = 'no',
           lock_photo = 'no',
           lock_member = 'no',
           lock_chat = 'no',
-          flood = 'yes'
+          lock_stickers = 'no',
+          flood = 'no'
         }
       }
       save_data(_config.moderation.data, data)
@@ -84,14 +88,16 @@ function check_member_group(cb_extra, success, result)
         settings = {
           set_name = string.gsub(msg.to.print_name, '_', ' '),
           lock_join = 'no',
-		  antifosh = 'yes',
-		  antiads = 'yes',
-		  lock_name = 'yes',
+          antifosh = 'no',
+	  antiads = 'no',
+          antilink = 'no',
+	  lock_name = 'no',
           lock_photo = 'no',
           lock_member = 'no',
           lock_chat = 'no',
-          flood = 'yes',
-        }
+          lock_stickers = 'no',
+          flood = 'no'
+}
       }
       save_data(_config.moderation.data, data)
       local groups = 'groups'
@@ -120,13 +126,15 @@ local function check_member_modadd(cb_extra, success, result)
         settings = {
           set_name = string.gsub(msg.to.print_name, '_', ' '),
           lock_join = 'no',
-		  antifosh = 'yes',
-		  antiads = 'yes',
-		  lock_name = 'yes',
+          antifosh = 'no',
+	  antiads = 'no',
+          antilink = 'no',
+	  lock_name = 'no',
           lock_photo = 'no',
           lock_member = 'no',
           lock_chat = 'no',
-          flood = 'yes',
+          lock_stickers = 'no',
+          flood = 'no'
         }
       }
       save_data(_config.moderation.data, data)
@@ -220,7 +228,11 @@ local function show_group_settingsmod(msg, data, target)
     	leave_ban = data[tostring(msg.to.id)]['settings']['leave_ban']
    	end
   local settings = data[tostring(target)]['settings']
-  local text = "›Group Settings:\n•••Kick new member with link : "..settings.lock_join.."\n•••Lock group Fosh : "..settings.antifosh.."\n•••Lock group chat : "..settings.lock_chat.."\n•••Lock group ads : "..settings.antiads.."\n•••Lock group name : "..settings.lock_name.."\n•••Lock group photo : "..settings.lock_photo.."\n•••kick new member : "..settings.lock_member.."\n•••Lock leave ban : "..leave_ban.."\n•••flood set on : "..NUM_MSG_MAX.."\n•••Bot can come : "..bots_protection.."                                        ›»DALAD People Version 2.3 (◉_◉)"
+  local text = "›Group Settings:\n•••Kick new member with link : "..settings.lock_join.."\n•••Lock group Fosh : "..settings.antifosh.."\n•••Lock group chat : 
+"..settings.antilink.."\n•••lock group link :
+"..settings.lock_chat.."\n•••Lock group ads : "..settings.antiads.."\n•••Lock group name : "..settings.lock_name.."\n•••Lock group photo : "..settings.lock_photo.."\n•••kick new member : "..settings.lock_member.."\n•••Lock leave ban :
+"..settings.lock_sticker.."\n•••lock group sticker :
+"..leave_ban.."\n•••flood set on : "..NUM_MSG_MAX.."\n•••Bot can come : "..bots_protection.."                                        ›»DALAD People Version 3 (◉_◉)"
   return text
 end
 
